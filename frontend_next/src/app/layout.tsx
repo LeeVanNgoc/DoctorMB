579519cfe8fa6e310ebede090daf14311aaa08b4
Lee Ngoc <lee.ngoc6462@gmail.com>
2026-08-08 22:08:16 +0700

    Fix file again

--- frontend_next/src/app/layout.tsx
+++ frontend_next/src/app/layout.tsx
@@ -36,7 +36,6 @@ export default function RootLayout({
         <AuthProvider>
           <Providers>
             {children}
-
             <Toaster />
           </Providers>
         </AuthProvider>

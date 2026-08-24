54372e5266b872d9e63a65ce0ef3d686ac82daf8
Lee Ngoc <lee.ngoc6462@gmail.com>
2026-08-24 15:48:25 +0700

    feat(users): complete user management

---
+++ backend_nest/src/common/interfaces/authenticated-request.interface.ts
@@ -0,0 +1,11 @@
+import { Request } from 'express';
+
+import { Role } from '../enums/role.enum';
+
+export interface AuthenticatedRequest extends Request {
+  user: {
+    userId: string;
+    email: string;
+    role: Role;
+  };
+}
